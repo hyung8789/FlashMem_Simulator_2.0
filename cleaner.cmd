cf7a0440e24f5b3fca3241df303e3becd4fcee1d
hyung8789 <44813321+hyung8789@users.noreply.github.com>
2020-08-20 17:19:21 +0900

    Update cleaner.cmd

--- cleaner.cmd
+++ cleaner.cmd
@@ -2,9 +2,15 @@
 PUSHD %~DP0
 set CURRENTPATH=%cd%
 
+if not exist "%CURRENTPATH%\플래시 메모리 시뮬레이터 2.0.exe" goto ERR
 del storage.bin
 del table.bin
 del table.txt
 del volume.txt
 del rr_read_index.txt
 del block_meta_output.txt
+
+:ERR
+echo Must be in the same folder with FlashMem Simulator
+pause
+exit
