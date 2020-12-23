9f99a8165b28b8cdafbdf90f39e9b8de89c95cdc
hyung8789 <44813321+hyung8789@users.noreply.github.com>
2020-12-23 18:02:38 +0900

    Delete Random_gen.cpp

--- Random_gen.cpp
+++
@@ -1,8 +0,0 @@
-#include "FlashMem.h"
-
-// Wear-leveling을 위한 무작위 오프셋 번호를 생성하는 난수 추출 함수 정의
-
-__int8 get_rand_offset() //0 ~ 31 분포 내 반환
-{ 
-	return (__int8)dist(gen); //분포 내의 메르센 트위스터에 의한 무작위 난수 반환
-}
